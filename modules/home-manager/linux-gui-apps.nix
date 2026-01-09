6cff6c8e6323541d6816bb8e0143ce7d766b5ee3
Arif Rezai <marezai10@gmail.com>
2026-01-10 00:11:23 +0900

    GUI apps

--- modules/home-manager/linux-gui-apps.nix
+++ modules/home-manager/linux-gui-apps.nix
@@ -8,4 +8,14 @@
       { id = "dbepggeogbaibhgnhhndojpepiihcmeb"; }
     ];
   };
+  programs.ghostty.enable = true;
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    profiles.defualt.extensions = with pkgs.vscode-extensions; [
+      dracula-theme.theme-dracula
+      vscodevim.vim
+      yzhang.markdown-all-in-one
+    ];
+  };
 }
--- modules/nixos/cosmic.nix
+++ modules/nixos/cosmic.nix
@@ -3,4 +3,9 @@
   services.displayManager.cosmic-greeter.enable = true;
   services.desktopManager.cosmic.enable = true;
   services.system76-scheduler.enable = true;
+
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    ELECTRON_OZONE_PLATFORM_HINT = "wayland";
+  };
 }
