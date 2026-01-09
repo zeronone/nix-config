52331e349948b2bffb64f371b34f923790c0ebd3
Arif Rezai <marezai10@gmail.com>
2026-01-10 00:30:02 +0900

    Fix vscodium

--- modules/home-manager/linux-gui-apps.nix
+++ modules/home-manager/linux-gui-apps.nix
@@ -11,11 +11,6 @@
   programs.ghostty.enable = true;
   programs.vscode = {
     enable = true;
-    package = pkgs.vscodium;
-    profiles.defualt.extensions = with pkgs.vscode-extensions; [
-      dracula-theme.theme-dracula
-      vscodevim.vim
-      yzhang.markdown-all-in-one
-    ];
+    package = pkgs.vscodium.fhs;
   };
 }
--- modules/nixos/cosmic.nix
+++ modules/nixos/cosmic.nix
@@ -4,6 +4,8 @@
   services.desktopManager.cosmic.enable = true;
   services.system76-scheduler.enable = true;
 
+  # This fixes scaling on Electron apps
+  # It also makes the text crisper on GUI apps
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
     ELECTRON_OZONE_PLATFORM_HINT = "wayland";
