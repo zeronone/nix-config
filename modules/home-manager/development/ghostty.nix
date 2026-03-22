c6f3ad1cdb965eeb2da0b0beed06d970f3284e81
Arif Rezai <marezai10@gmail.com>
2026-03-23 08:58:49 +0900

    Updated

--- modules/home-manager/development/ghostty.nix
+++ modules/home-manager/development/ghostty.nix
@@ -10,6 +10,9 @@
       macos-option-as-alt = true;
       mouse-scroll-multiplier = 1.0;
 
+      # We rely on tmux scrollback
+      scrollback-limit = 0;
+
       # This breaks paperwm.spoon
       # macos-titlebar-style = "hidden";
       gtk-titlebar = false;
@@ -20,39 +23,38 @@
         "performable:ctrl+c=copy_to_clipboard"
         "performable:super+c=copy_to_clipboard"
         "super+v=paste_from_clipboard"
-        # When 1.2.4 is released
-        # # Activate vim mode
-        # "alt+v=activate_key_table:vim"
-        # # Key table definition
-        # "vim/"
-        # # Line movement
-        # "vim/j=scroll_page_lines:1"
-        # "vim/k=scroll_page_lines:-1"
-        # # Page movement
-        # "vim/ctrl+d=scroll_page_down"
-        # "vim/ctrl+u=scroll_page_up"
-        # "vim/ctrl+f=scroll_page_down"
-        # "vim/ctrl+b=scroll_page_up"
-        # "vim/shift+j=scroll_page_down"
-        # "vim/shift+k=scroll_page_up"
-        # # Jump to top/bottom
-        # "vim/g>g=scroll_to_top"
-        # "vim/shift+g=scroll_to_bottom"
-        # # Search (if you want vim-style search entry)
-        # "vim/slash=start_search"
-        # "vim/n=navigate_search:next"
-        # # Copy mode / selection
-        # # Note we're missing a lot of actions here to make this more full featured.
-        # "vim/v=copy_to_clipboard"
-        # "vim/y=copy_to_clipboard"
-        # # Command Palette
-        # "vim/shift+semicolon=toggle_command_palette"
-        # # Exit
-        # "vim/escape=deactivate_key_table"
-        # "vim/q=deactivate_key_table"
-        # "vim/i=deactivate_key_table"
-        # # Catch unbound keys
-        # "vim/catch_all=ignore"
+        # Activate vim mode
+        "alt+v=activate_key_table:vim"
+        # Key table definition
+        "vim/"
+        # Line movement
+        "vim/j=scroll_page_lines:1"
+        "vim/k=scroll_page_lines:-1"
+        # Page movement
+        "vim/ctrl+d=scroll_page_down"
+        "vim/ctrl+u=scroll_page_up"
+        "vim/ctrl+f=scroll_page_down"
+        "vim/ctrl+b=scroll_page_up"
+        "vim/shift+j=scroll_page_down"
+        "vim/shift+k=scroll_page_up"
+        # Jump to top/bottom
+        "vim/g>g=scroll_to_top"
+        "vim/shift+g=scroll_to_bottom"
+        # Search (if you want vim-style search entry)
+        "vim/slash=start_search"
+        "vim/n=navigate_search:next"
+        # Copy mode / selection
+        # Note we're missing a lot of actions here to make this more full featured.
+        "vim/v=copy_to_clipboard"
+        "vim/y=copy_to_clipboard"
+        # Command Palette
+        "vim/shift+semicolon=toggle_command_palette"
+        # Exit
+        "vim/escape=deactivate_key_table"
+        "vim/q=deactivate_key_table"
+        "vim/i=deactivate_key_table"
+        # Catch unbound keys
+        "vim/catch_all=ignore"
       ];
     };
     themes = {
