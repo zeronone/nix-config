a3ab3efc6bde0f0f8500b3c4957dcf91e0ee8df7
Arif Rezai <marezai10@gmail.com>
2026-04-20 16:01:36 +0900

    chore: add mh for midori-labs

--- machines/nixos/asahi-nixos/home.nix
+++ machines/nixos/asahi-nixos/home.nix
@@ -1,4 +1,4 @@
-{ myHmModules, ... }:
+{ myHmModules, username, ... }:
 {
   imports = [
     myHmModules.apple-us-iso-fcitx5
@@ -6,4 +6,20 @@
     myHmModules.node
     myHmModules.keepassxc
   ];
+
+  users.users."${username}".linger = true;
+  systemd.user.services.midori-harness = {
+    Unit = {
+      Description = "Midori Harness - midori-labs";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+    Service = {
+      WorkingDirectory = "/home/arif/midori-labs";
+      ExecStart = "/home/arif/.local/bin/midori-harness run";
+      Restart = "always";
+      RestartSec = "3";
+    };
+  };
 }
